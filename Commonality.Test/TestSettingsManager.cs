c7ad1e5feddd62b6d32817e50dc93add4d02b869
James Coliz <jcoliz@mail.com>
2018-03-24 13:13:31 -0700

    Clock to 100% tests

---
+++ Commonality.Test/TestSettingsManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commonality.Test.Helpers
+{
+    public class TestSettingsManager : ISettings
+    {
+        Dictionary<string, IEnumerable<string>> Values = new Dictionary<string, IEnumerable<string>>();
+
+        public IEnumerable<string> GetCompositeKey(string name)
+        {
+            if (Values.ContainsKey(name))
+                return Values[name];
+            else
+                return null;
+        }
+
+        public string GetKey(string name)
+        {
+            if (Values.ContainsKey(name))
+                return Values[name].First();
+            else
+                return null;
+        }
+
+        public void SetCompositeKey(string name, IEnumerable<string> values)
+        {
+            Values[name] = values;
+        }
+
+        public void SetKey(string name, string value)
+        {
+            Values[name] = new[] { value };
+        }
+    }
+}
