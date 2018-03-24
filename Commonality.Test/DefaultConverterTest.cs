3c0b76e1119096eeb06b2b3e2cd0e8b1d77dcd6b
James Coliz <jcoliz@mail.com>
2018-03-23 21:11:13 -0700

    New default converter test

---
+++ Commonality.Test/DefaultConverterTest.cs
@@ -0,0 +1,34 @@
+﻿using Commonality.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    public class DefaultConverterTest
+    {
+        private DefaultConverter Converter;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Converter = new DefaultToBoolConverter();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(Converter);
+        }
+    }
+
+    class DefaultToBoolConverter : DefaultConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter)
+        {
+            return base.Convert<bool>(true,false,value,targetType,parameter);
+        }
+    }
+}
