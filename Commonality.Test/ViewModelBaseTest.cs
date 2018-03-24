c98e87b03704269ffc82adc5a2783b5434cc9a28
James Coliz <jcoliz@mail.com>
2018-03-23 23:04:57 -0700

    Viewmodel base stub test

---
+++ Commonality.Test/ViewModelBaseTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    public class ViewModelBaseTest
+    {
+        private ViewModelBase ModelBase;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            ModelBase = new ViewModelBase();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(ModelBase);
+        }
+    }
+}
