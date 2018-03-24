fa922fa69d15d76365e72aacbea68383adc2defa
James Coliz <jcoliz@mail.com>
2018-03-24 09:45:36 -0700

    Stub tests for dot net clock

---
+++ Commonality.Test/DotNetClockTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    public class DotNetClockTest
+    {
+        private Clock ThisClock;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            ThisClock = new Clock();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(ThisClock);
+        }
+    }
+}
