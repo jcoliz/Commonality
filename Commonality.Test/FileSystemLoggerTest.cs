7bf3d9d8d802f3d1a48474ada932e792c0e52815
James Coliz <jcoliz@mail.com>
2018-03-24 13:35:32 -0700

    New stub tests for filesystem logger

---
+++ Commonality.Test/FileSystemLoggerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    class FileSystemLoggerTest
+    {
+        private FileSystemLogger Logger;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Logger = new FileSystemLogger();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(Logger);
+        }
+
+    }
+}
