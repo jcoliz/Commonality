d49d17bc9f9f2d1109b686c78a33b568f733ed2a
James Coliz <jcoliz@mail.com>
2018-03-23 17:49:05 -0700

    First real test. And enable correct amount of debbing information for code coverage.

---
+++ Commonality.Test/DateFormatConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using Commonality.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    public class DateFormatConverterTest
+    {
+        private DateFormatConverter Converter;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Converter = new DateFormatConverter();
+        }
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(Converter);
+        }
+    }
+}
