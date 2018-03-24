6dc2c1d1346137c9ac5b363d4b7e257c11f9e38a
James Coliz <jcoliz@mail.com>
2018-03-23 22:37:00 -0700

    Stub delegatecommand test

---
+++ Commonality.Test/DelegateCommandTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commonality.Test
+{
+    [TestClass]
+    public class DelegateCommandTest
+    {
+        private DelegateCommand Command;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Command = new DelegateCommand(Action);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(Command);
+        }
+
+        private void Action(object parameter)
+        {
+
+        }
+    }
+}
